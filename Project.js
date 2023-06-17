ee8d943187a0e71c9895620c9d63b35e158d8768
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2023-06-17 12:47:06 +0530

    Project.js

---
+++ Project.js
@@ -0,0 +1,9 @@
+function getValues() {
+    let ele1 = document.getElementById("userFName").value;
+    let ele2 = document.getElementById("userLName").value;
+    alert(`your full name - ${ele1} ${ele2}`)
+
+
+
+
+}
